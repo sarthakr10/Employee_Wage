f97b17985470caa54ef4c77c991231b62ffe8c4d
sarthakr10 <rastogisarthak35@gmail.com>
2025-03-20 11:50:02 +0530

    UC6

---
+++ Employee_Storingdailywage.js
@@ -0,0 +1,33 @@
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+function calcDailyWage(empHrs){
+    return empHrs *WAGE_PER_HOUR;
+}
+const MAX_HRS_IN_MONTH=160;
+const NUM_OF_WORKING_DAYS=20;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyWageArr=new Array();
+while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck=Math.floor(Math.random()*10)%3;
+    let empHrs=getWorkingHours(empCheck);
+    totalEmpHrs+=empHrs;
+    empDailyWageArr.push(calcDailyWage(empCheck));
+}
+let empWage=calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: "+totalEmpHrs);
+console.log("UC6-Total Days "+totalWorkingDays +"Total hrs "+totalEmpHrs+ " Emp Wage: "+empWage);
